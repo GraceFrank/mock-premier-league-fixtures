43e2432fff2e401dd3908d64208e3cfaa0941b50
Grace Frank <frankgracey@gmail.com>
2019-09-14 00:00:52 +0100

    added api config file for all config from env file

---
+++ config/config.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+//all configurations for the app read from the .env file
+module.exports = {
+  apiPort = process.env.API_PORT,
+  databaseUrl = process.env.DATABASE_URL,
+}
