c6676227c28a5795eaac92a84564ca44aa3176b8
Grace Frank <frankgracey@gmail.com>
2019-09-15 10:32:51 +0100

    updated app congigurations

--- config/config.js
+++ config/config.js
@@ -3,5 +3,7 @@ require('dotenv').config();
 //all configurations for the app read from the .env file
 module.exports = {
   apiPort: process.env.API_PORT,
-  databaseUrl: process.env.DATABASE_URL
+  databaseUrl: process.env.DATABASE_URL,
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT
 };
