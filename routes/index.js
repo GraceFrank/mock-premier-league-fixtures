c1573ab9887a732f29e3248647b9c801dd690508
Grace Frank <frankgracey@gmail.com>
2019-09-22 03:09:06 +0100

    added fixture route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const bodyParser = require('body-parser');
 const users = require('./users-route');
 const auth = require('./auth-route');
+const teams = require('./teams-route');
+const fixtures = require('./fixtures-route');
 
 const routes = app => {
   app.use(bodyParser.json());
@@ -8,8 +10,8 @@ const routes = app => {
 
   app.use('/api/v1/users', users);
   app.use('/api/v1/auth', auth);
-  // app.use('/api/v1/teams');
-  // app.use('api/v1/fixtures');
+  app.use('/api/v1/teams', teams);
+  app.use('/api/v1/fixtures', fixtures);
 };
 
 module.exports = routes;
